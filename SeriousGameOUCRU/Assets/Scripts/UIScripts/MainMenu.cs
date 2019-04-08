56fa12f56e0a419552e985d9c2c6a257da9397ff
Unknown <lywin.shunayte@gmail.com>
2019-04-08 15:44:03 +0700

    Added small animation on play button

---
+++ SeriousGameOUCRU/Assets/Scripts/UIScripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    /*** PUBLIC VARIABLES ***/
+
+    public Animator animator;
+
+
+    /***** MONOBEHAVIOUR FUNCTIONS *****/
+
+    void Start()
+    {
+        
+    }
+
+
+    /***** SIZE FUNCTIONS *****/
+
+    public void GrowText()
+    {
+        animator.SetBool("grow", true);
+    }
+
+    public void ShrinkText()
+    {
+        animator.SetBool("grow", false);
+    }
+}
