7e03f4329afda1e0d25613f4d5bb3da765de319e
Unknown <lywin.shunayte@gmail.com>
2019-05-10 12:01:42 +0700

    Fixed background clipping

--- SeriousGameOUCRU/Assets/Scripts/Parallax.cs
+++ SeriousGameOUCRU/Assets/Scripts/Parallax.cs
@@ -13,8 +13,8 @@ public class Parallax : MonoBehaviour
     /*** PRIVATE VARIABLES ***/
 
     private Vector3 startPos;
-    private float lenX = 80f;
-    private float lenZ = 40f;
+    private float lenX = 100f;
+    private float lenZ = 50f;
 
 
     /***** MONOBEHAVIOUR FUNCTIONS *****/
