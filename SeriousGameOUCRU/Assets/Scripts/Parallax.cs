5b93b6735bce13262be1cb17470d17e875433c6b
Unknown <lywin.shunayte@gmail.com>
2019-07-05 15:25:22 +0700

    Background parallax now handled in shader

--- SeriousGameOUCRU/Assets/Scripts/Parallax.cs
+++ SeriousGameOUCRU/Assets/Scripts/Parallax.cs
@@ -7,13 +7,14 @@ public class Parallax : MonoBehaviour
     /*** PUBLIC VARIABLES ***/
 
     public GameObject cam;
-    public float parallaxEffect;
 
 
     /*** PRIVATE VARIABLES ***/
 
     private Vector3 startPos;
     private Vector2 length;
+
+    private float parallaxEffect;
 
 
     /***** MONOBEHAVIOUR FUNCTIONS *****/
@@ -22,25 +23,42 @@ public class Parallax : MonoBehaviour
     {
         startPos = transform.position;
         length = GetComponent<SpriteRenderer>().bounds.size;
+
+        parallaxEffect = GetComponent<SpriteRenderer>().sharedMaterial.GetFloat("_ParallaxEffect");
     }
 
-    void FixedUpdate()
+    void Update()
     {
         float tempX = cam.transform.position.x * (1 - parallaxEffect);
         float tempZ = cam.transform.position.y * (1 - parallaxEffect);
 
-        float distX = cam.transform.position.x * parallaxEffect;
-        float distZ = cam.transform.position.y * parallaxEffect;
-
-        // Affect new position
-        transform.position = new Vector3(startPos.x + distX, startPos.y + distZ, startPos.z);
-
         // Adjust X start position
-        if (tempX > startPos.x + length.x / 2) startPos.x += length.x;
-        else if (tempX < startPos.x - length.x / 2) startPos.x -= length.x;
+        if (tempX > startPos.x + length.x / 2)
+        {
+            startPos.x += length.x; 
+            Replace();
+        }else if (tempX < startPos.x - length.x / 2) 
+        {
+            startPos.x -= length.x; 
+            Replace();
+        }
 
         // Adjust Y start position
-        if (tempZ > startPos.y + length.y / 2) startPos.y += length.y;
-        else if (tempZ < startPos.y - length.y / 2) startPos.y -= length.y;
+        if (tempZ > startPos.y + length.y / 2) 
+        {
+            startPos.y += length.y;
+            Replace();
+            
+        }
+        else if (tempZ < startPos.y - length.y / 2) 
+        {
+            startPos.y -= length.y;
+            Replace();
+        }
+    }
+
+    void Replace()
+    {
+        transform.position = startPos;
     }
 }
